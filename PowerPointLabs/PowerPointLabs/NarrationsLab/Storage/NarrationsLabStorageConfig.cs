e9482cd05feb164d8647851ac768206765e55e86
LiXiaowei <lixiaowei0535@gmail.com>
2018-11-06 12:42:49 +0800

    storage work in progress

---
+++ PowerPointLabs/PowerPointLabs/NarrationsLab/Storage/NarrationsLabStorageConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PowerPointLabs.NarrationsLab.Data;
+
+namespace PowerPointLabs.NarrationsLab.Storage
+{
+    public static class NarrationsLabStorageConfig
+    {
+        private const string defaultNarrationsStorageFolder = "PowerPointLabs Narrations Access Key Storage";
+        private const string defaultNarrationsStorageFile = "useraccount.xml";
+        private static string defaultApplicationFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public static string GetAccessKeyStoragePath()
+        {
+            return Path.Combine(defaultApplicationFolderPath, defaultNarrationsStorageFolder);
+        }
+
+        public static string GetAccessKeyFilePath()
+        {
+            return Path.Combine(defaultApplicationFolderPath, defaultNarrationsStorageFolder, defaultNarrationsStorageFile);
+        }
+
+        public static void SaveUserAccount(UserAccount account)
+        {
+            if (!Directory.Exists(GetAccessKeyStoragePath()))
+            {
+                Directory.CreateDirectory(GetAccessKeyStoragePath());
+            }
+            if (!File.Exists(GetAccessKeyFilePath()))
+            {
+                File.Create(GetAccessKeyFilePath());
+            }
+        }
+    }
+}
